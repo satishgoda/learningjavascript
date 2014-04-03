8606843180167a91a92880a3f0e27c960c7696da
Satish Goda <satishgoda@gmail.com>
2014-04-03 09:36:57 +0530

    Create first.js

---
+++ tools/webconsole/first.js
@@ -0,0 +1,51 @@
+[09:16:01.967] The character encoding of the HTML document was not declared. The document will render with garbled text in some browser configurations if the document contains characters from outside the US-ASCII range. The character encoding of the page must be declared in the document or in the transfer protocol. @ file:///home/satishg/Documents/js_1.html
+--
+[09:16:27.621] document
+[09:16:27.625] [object HTMLDocument]
+--
+[09:16:35.988] document.namespaceURI
+[09:16:35.991] null
+--
+[09:16:43.380] document.URL
+[09:16:43.383] "file:///home/satishg/Documents/js_1.html"
+--
+[09:16:59.572] document.baseURI
+[09:16:59.575] "file:///home/satishg/Documents/js_1.html"
+--
+[09:17:13.828] document.contentType
+[09:17:13.832] "text/html"
+--
+[09:19:20.846] document.compatMode
+[09:19:20.849] "CSS1Compat"
+--
+[09:19:38.718] document.activeElement
+[09:19:38.721] [object HTMLBodyElement]
+--
+[09:19:54.734] document.activeElement
+[09:19:54.738] [object HTMLButtonElement]
+--
+[09:20:04.462] document.activeElement
+[09:20:04.465] [object HTMLBodyElement]
+--
+[09:20:15.062] document.activeElement
+[09:20:15.065] [object HTMLButtonElement]
+--
+[09:20:31.126] document.attributes
+[09:20:31.130] null
+--
+[09:20:41.271] document.anchors
+[09:20:41.274] [object HTMLCollection]
+--
+[09:27:52.308] document.childNodes
+[09:27:52.312] ({0:({}), 1:({})})
+--
+[09:27:58.716] document.childNodes[0]
+[09:27:58.719] [object DocumentType]
+[09:28:01.276] document.childNodes[1]
+[09:28:01.279] [object HTMLHtmlElement]
+--
+[09:20:56.439] for anchor in document.anchors
+[09:20:56.442] SyntaxError: missing ( after for
+--
+[09:21:06.263] for (anchor in document.anchors)
+[09:21:06.266] SyntaxError: syntax error
