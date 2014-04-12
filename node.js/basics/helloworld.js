60b875287baecf652beb228067b8224d6af70e7f
Satish Goda <satishgoda@gmail.com>
2014-04-12 19:35:58 +0530

    Update helloworld.js
    Experiencing my n00b javascript syntax

--- node.js/basics/helloworld.js
+++ node.js/basics/helloworld.js
@@ -1,8 +1,13 @@
 var http = require('http');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
+function hello_world_server (req, res) 
+{
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n'+Date());
+}
+
+var server = http.createServer(hello_world_server)
+
+server.listen(1337, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:1337/');
