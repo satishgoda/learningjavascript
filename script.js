78e22e191fa1f3d3082e093331bf345895b8ec1e
Glitch (learning-babylon-js-1) <none>
2020-04-17 15:40:50 +0000

    📕❄️ Removed comments

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-var canvas = document.getElementById("renderCanvas"); // Get the canvas element
-var engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
+var canvas = document.getElementById("renderCanvas"); 
+var engine = new BABYLON.Engine(canvas, true); 
 
-/******* Add the create scene function ******/
+
 var createScene = function() {
-  // Create the scene space
   var scene = new BABYLON.Scene(engine);
 
-  // Add a camera to the scene and attach it to the canvas
   var camera = new BABYLON.ArcRotateCamera(
     "Camera",
     Math.PI / 2,
@@ -17,19 +15,18 @@ var createScene = function() {
   );
   camera.attachControl(canvas, true);
 
-  // Add lights to the scene
   var light1 = new BABYLON.HemisphericLight(
     "light1",
     new BABYLON.Vector3(1, 1, 0),
     scene
   );
+
   var light2 = new BABYLON.PointLight(
     "light2",
     new BABYLON.Vector3(0, 1, -1),
     scene
   );
 
-  // Add and manipulate meshes in the scene
   var sphere = BABYLON.MeshBuilder.CreateSphere(
     "sphere",
     { diameter: 2 },
@@ -38,16 +35,15 @@ var createScene = function() {
 
   return scene;
 };
-/******* End of the create scene function ******/
 
-var scene = createScene(); //Call the createScene function
 
-// Register a render loop to repeatedly render the scene
+var scene = createScene();
+
 engine.runRenderLoop(function() {
   scene.render();
 });
 
-// Watch for browser/canvas resize events
+
 window.addEventListener("resize", function() {
   engine.resize();
 });
