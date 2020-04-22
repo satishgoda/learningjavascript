1c235ea861b65b6feb818cbb9146d522d07daa10
Glitch (learning-baylon-js-2) <none>
2020-04-22 01:40:13 +0000

    🎰🚔 Clickable links

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+Babylon.js : Babylon 101 : First Steps
+
+- https://doc.babylonjs.com/babylon101/
+
 Babylon.js : Babylon 101 : Discover Basic Elements
 
-https://doc.babylonjs.com/babylon101/discover_basic_elements
+- https://doc.babylonjs.com/babylon101/discover_basic_elements
+- https://www.babylonjs-playground.com/#INB624#1
+- https://doc.babylonjs.com/api/classes/babylon.debug.axesviewer
--- script.js
+++ script.js
@@ -1,74 +1,78 @@
-var canvas = document.getElementById("renderCanvas"); 
-var engine = new BABYLON.Engine(canvas, true); 
+var canvas = document.getElementById("renderCanvas");
 
 
-var createScene = function() {
-  var scene = new BABYLON.Scene(engine);
-
-  var axes = new BABYLON.Debug.AxesViewer(scene);
-  
-  var camera = new BABYLON.ArcRotateCamera(
-    "Camera",
-    Math.PI / 2,
-    Math.PI / 2,
-    2,
-    new BABYLON.Vector3(-1, 1, 2),
-    scene
-  );
-  camera.attachControl(canvas, true);
-
-  var light1 = new BABYLON.HemisphericLight(
-    "light1",
-    new BABYLON.Vector3(1, 1, 1),
-    scene
-  );
-
-  var plane = new BABYLON.MeshBuilder.CreatePlane(
-    "plane",
-    {size: 1},
-    scene
-  );
-  
-  var sourcePlane = new BABYLON.Plane(0, 1, 0, 0);
-  sourcePlane.normalize()
-  
-  var groundplane = new BABYLON.MeshBuilder.CreatePlane(
-    "plane",
-    {
-      size: 5,
-      sourcePlane: sourcePlane,
-      sideOrientation: BABYLON.Mesh.DOUBLESIDE
-    },
-    scene
-  );
-  
-  var box = new BABYLON.MeshBuilder.CreateBox(
-    "box",
-    {
-      faceColors: [
-        new BABYLON.Color4(1, 0, 0, 1),
-        new BABYLON.Color4(0, 1, 0, 1),
-        new BABYLON.Color4(0, 0, 1, 1),
-        new BABYLON.Color4(1, 1, 0, 1),
-        new BABYLON.Color4(1, 0, 1, 1),
-        new BABYLON.Color4(0, 1, 1, 1),
-      ],
-    },
-    scene
-  );
-  
-  box.setPositionWithLocalVector(new BABYLON.Vector3(0, .5, 0));
-
-  return scene;
-};
+var engine = new BABYLON.Engine(canvas, true);
 
 
-var scene = createScene();
+var scene = new BABYLON.Scene(engine);
+
+
+var axes = new BABYLON.Debug.AxesViewer(scene);
+
+
+var camera = new BABYLON.ArcRotateCamera(
+  "Camera",
+  Math.PI / 2,
+  Math.PI / 2,
+  2,
+  new BABYLON.Vector3(0, 0, 0),
+  scene
+);
+camera.setPosition(new BABYLON.Vector3(-2, 1, 3));
+camera.attachControl(canvas, true);
+
+
+var light1 = new BABYLON.HemisphericLight(
+  "light1",
+  new BABYLON.Vector3(1, 1, 1),
+  scene
+);
+
+
+var sourcePlane = new BABYLON.Plane(0, 1, 0, 0);
+sourcePlane.normalize();
+
+var groundplane = new BABYLON.MeshBuilder.CreatePlane(
+  "plane",
+  {
+    size: 15,
+    sourcePlane: sourcePlane,
+    sideOrientation: BABYLON.Mesh.DOUBLESIDE
+  },
+  scene
+);
+
+var box = new BABYLON.MeshBuilder.CreateBox(
+  "box",
+  {
+    faceColors: [
+      new BABYLON.Color4(1, 0, 0, 1),
+      new BABYLON.Color4(0, 1, 0, 1),
+      new BABYLON.Color4(0, 0, 1, 1),
+      new BABYLON.Color4(1, 1, 0, 1),
+      new BABYLON.Color4(1, 0, 1, 1),
+      new BABYLON.Color4(0, 1, 1, 1)
+    ]
+  },
+  scene
+);
+
+box.setPositionWithLocalVector(new BABYLON.Vector3(0, 0.5, 0));
+
+box.actionManager = new BABYLON.ActionManager(scene);
+box.URL = "https://doc.babylonjs.com/api/classes/babylon.debug.axesviewer";
+box.actionManager.registerAction(
+  new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPickTrigger, function(
+    event
+  ) {
+    var box = event.meshUnderPointer;
+    window.open(box.URL);
+  })
+);
 
 engine.runRenderLoop(function() {
   scene.render();
 });
-
 
 window.addEventListener("resize", function() {
   engine.resize();
