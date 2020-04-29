12ea653c6d8b4dbd01fdf30ca959a6f0c6e320d8
Satish Goda <satishgoda@live.com>
2020-04-29 12:21:26 -0700

    Create updating_arrays_and_objects.js
    https://svelte.dev/tutorial/updating-arrays-and-objects

---
+++ svelte/reactivity/updating_arrays_and_objects.js
@@ -0,0 +1,29 @@
+<script>
+	let numbers = [1, 2, 3, 4];
+
+	function addNumber() {
+		//numbers.push(numbers.length + 1);
+		numbers = [...numbers, numbers.length+1]
+	}
+
+	function removeNumber() {
+		numbers.pop();
+		if (numbers.length === 0) {
+			numbers = [1]
+		}else{
+			numbers = numbers
+		}
+	}
+	
+	$: sum = numbers.reduce((t, n) => t + n, 0);
+</script>
+
+<p>{numbers.join(' + ')} = {sum}</p>
+
+<button on:click={addNumber}>
+	Add a number
+</button>
+
+<button on:click={removeNumber}>
+	Remove a number
+</button>
