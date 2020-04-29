23ea77c34644d012030e671f003b5463d18e4ad4
Satish Goda <satishgoda@live.com>
2020-04-29 15:55:02 -0700

    enumerating using the each block

--- svelte/logic/each_block.js
+++ svelte/logic/each_block.js
@@ -9,9 +9,9 @@
 <h1>The Famous Cats of YouTube</h1>
 
 <ul>
-	{#each cats as cat}
+	{#each cats as cat, index}
 		<li><a target="_blank" href="https://www.youtube.com/watch?v={cat.id}">
-			{cat.name}
+			{cat.name} ({index})
 		</a></li>
 	{/each}
 </ul>
