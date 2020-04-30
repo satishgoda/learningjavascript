1cb8e9bc79c8831cd963c349a6f2b2554030c102
Satish Goda <satishgoda@live.com>
2020-04-29 21:46:56 -0700

    Create dom_events.js
    https://svelte.dev/tutorial/dom-events

---
+++ svelte/events/dom_events.js
@@ -0,0 +1,21 @@
+<script>
+	let m = { x: 0, y: 0 };
+
+	function handleMousemove(event) {
+		m.x = event.clientX;
+		m.y = event.clientY;
+	}
+	
+	$: {
+		console.clear();
+		console.log(m);
+	}
+</script>
+
+<style>
+	div { width: 100%; height: 100%; }
+</style>
+
+<div on:mousemove={handleMousemove}>
+	The mouse position is {m.x} x {m.y}
+</div>
